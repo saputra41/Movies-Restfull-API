7248adcf0e05db26ad4923bffacbf18fb13b0edf
Surya Saputra <surya.saputra030090@gmail.com>
2023-07-23 16:01:20 +0700

    penambahan try catch

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\API;
 
+use Throwable;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -17,21 +20,34 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
+
+        if ($validator->fails()) {
+            $response =  [
+                "response" => false,
+                "message" => "Invalid request data",
+                "details" => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
         $credentials = $request->only('email', 'password');
         $token = Auth::attempt($credentials);
 
         if (!$token) {
-            return response()->json([
+            $response = [
+                "response" => false,
                 'message' => 'Unauthorized',
-            ], 401);
+            ];
+            return response()->json($response, 401);
         }
 
         $user = Auth::user();
         return response()->json([
+            "response" => true,
             'user' => $user,
             'authorization' => [
                 'token' => $token,
@@ -42,40 +58,86 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        if ($validator->fails()) {
+            $response =  [
+                "response" => false,
+                "message" => "Invalid request data",
+                "details" => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
 
-        return response()->json([
-            'message' => 'User created successfully',
-            'user' => $user
-        ]);
+        try {
+            DB::beginTransaction();
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            DB::commit();
+
+            $response = [
+                'response' => true,
+                'message' => 'User created successfully',
+                'user' => $user
+            ];
+            return response()->json($response, 201);
+        } catch (Throwable $th) {
+            DB::rollback();
+
+            $response = [
+                "response" => false,
+                "message" => $th->getMessage(),
+            ];
+
+            return response()->json($response, 500);
+        }
     }
 
     public function logout()
     {
-        Auth::logout();
-        return response()->json([
-            'message' => 'Successfully logged out',
-        ]);
+        try {
+            Auth::logout();
+            $response = [
+                'response' => true,
+                'message' => 'Successfully logged out',
+            ];
+            return response()->json($response);
+        } catch (Throwable $th) {
+            $response = [
+                "response" => false,
+                "message" => $th->getMessage(),
+            ];
+
+            return response()->json($response, 500);
+        }
     }
 
     public function refresh()
     {
-        return response()->json([
-            'user' => Auth::user(),
-            'authorisation' => [
-                'token' => Auth::refresh(),
-                'type' => 'bearer',
-            ]
-        ]);
+        try {
+            return response()->json([
+                'user' => Auth::user(),
+                'authorisation' => [
+                    'token' => Auth::refresh(),
+                    'type' => 'bearer',
+                ]
+            ]);
+        } catch (Throwable $th) {
+            $response = [
+                "response" => false,
+                "message" => $th->getMessage(),
+            ];
+
+            return response()->json($response, 500);
+        }
     }
 }
