a12aea21640a6f1c6468cec94686c75f774eb9a8
Surya Saputra <surya.saputra030090@gmail.com>
2023-07-23 16:36:49 +0700

    test migrate

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations before tests
+        Artisan::call('migrate', ['--env' => 'testing']);
+    }
 }
